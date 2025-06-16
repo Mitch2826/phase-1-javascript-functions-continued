6b6c26416cbddc525d1e7fe4b851e628a5a1bc4f
maranimitchelle@gmail.com <mitch@DESKTOP-PMCR53J.localdomain>
2025-06-16 12:29:54 +0300

    Functions

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+//function expressions
+let mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork("work from home");
+
+//function level scope
+let wrapAdjective = function (flair = "*") {
+    return function (par1 = "special") {
+        return `You are ${flair}${par1}${flair}!`;
+    }
+}
+wrapAdjective("||");
